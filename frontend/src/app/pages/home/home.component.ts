3fda3e27a84cd9deddfdbfb315ae59402270a435
Santiago Arbelaez Contreras <santiago.arbelaezc@uqvirtual.edu.co>
2026-07-01 10:36:21 -0500

    testimonial card

---
+++ frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HeroComponent } from '../../shared/hero/hero.component';
+import { AboutComponent } from '../../shared/about/about.component';
+import { ServiceCardComponent } from '../../components/service-card/service-card.component';
+import { ProjectCardComponent } from '../../components/project-card/project-card.component';
+import { TestimonialCardComponent } from '../../components/testimonial-card/testimonial-card.component';
+import { ProyectosService } from '../../services/proyectos.service';
+import { ServiciosService } from '../../services/servicios.service';
+import { TestimoniosService } from '../../services/testimonios.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    HeroComponent,
+    AboutComponent,
+    ServiceCardComponent,
+    ProjectCardComponent,
+    TestimonialCardComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  readonly proyectosService = inject(ProyectosService);
+  readonly serviciosService = inject(ServiciosService);
+  readonly testimoniosService = inject(TestimoniosService);
+
+  readonly servicios = this.serviciosService.getServicios();
+  readonly testimonios = this.testimoniosService.getTestimonios();
+  readonly categorias = this.proyectosService.getCategorias();
+
+  onSelectCategoria(cat: string): void {
+    this.proyectosService.setCategoria(cat);
+  }
+}

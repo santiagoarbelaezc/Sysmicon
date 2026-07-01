469e1bbac262470a83275f390777759280e321bf
Santiago Arbelaez Contreras <santiago.arbelaezc@uqvirtual.edu.co>
2026-07-01 10:35:55 -0500

    testimonial card

---
+++ frontend/src/app/components/testimonial-card/testimonial-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Testimonio } from '../../models/testimonio.model';
+
+@Component({
+  selector: 'app-testimonial-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './testimonial-card.component.html',
+  styleUrl: './testimonial-card.component.css'
+})
+export class TestimonialCardComponent {
+  @Input({ required: true }) testimonio!: Testimonio;
+
+  getStarsArray(): number[] {
+    return Array(this.testimonio.calificacion).fill(0);
+  }
+}

46734568c97dd9e61ec0754d311435df76349bdb
Santiago Arbelaez Contreras <santiago.arbelaezc@uqvirtual.edu.co>
2026-07-02 21:18:07 -0500

    mejorando estilo de carga

--- frontend/src/app/pages/crea-tu-diseno/crea-tu-diseno.component.ts
+++ frontend/src/app/pages/crea-tu-diseno/crea-tu-diseno.component.ts
@@ -44,7 +44,7 @@ export class CreaTuDisenoComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.loadingTimer = setTimeout(() => {
       this.isLoadingScreen.set(false);
-    }, 5000);
+    }, 2000);
   }
 
   ngOnDestroy(): void {
